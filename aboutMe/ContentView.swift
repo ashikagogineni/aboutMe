96dec4cfdbf67627e8f7c50b7a01ef66b3a873a4
Ashika G <ashi.gogineni@gmail.com>
2023-07-13 14:08:37 -0700

    testtest

--- aboutMe/ContentView.swift
+++ aboutMe/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textTitle1 = ""
+    @State private var textTitle2 = ""
+    @State private var name = "Ashika"
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack {
+                Image("sanjose")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                    .padding()
+                Image("sanjose2")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                    .padding()
+            }
+            Text(name)
+                .font(.title2)
+                .multilineTextAlignment(.center)
+            VStack {
+                Button("fun facts about me!") {
+                    textTitle1 = "my favorite color is blue and I'm a senior in high school!"
+                }
+                Text(textTitle1)
+                .font(.title3)
+                .buttonStyle(.borderedProminent)
+                .tint(.gray)
+                .padding()
+                .background(Rectangle() .foregroundColor(.white))
+                .cornerRadius(15)
+                .shadow(radius: 15)
+                .padding()
+                
+            }
         }
-        .padding()
     }
 }
 
